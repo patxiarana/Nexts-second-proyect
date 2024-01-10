ce933c0a0e11e62e1e6602d5f7fe92e5166c048c
patxi <patxiarana05@gmail.com>
2024-01-10 08:03:14 -0300

    funcion get implementada redireccionamiento implementado

--- nextjs-prisma-crud/src/app/new/page.jsx
+++ nextjs-prisma-crud/src/app/new/page.jsx
@@ -1,20 +1,39 @@
+"use client"
+import { useRouter } from "next/navigation"
 
 
-import React from 'react'
 
 export default function NewPage() {
+  const router = useRouter()
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const title = e.target.title.value
+    const description = e.target.description.value
+    const res = await fetch('api/task', {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json();
+  console.log(data)
+  router.push("/")
+  }
+
+
   return (
     <div className='h-screen flex justify-center items-center'>
-      <form className='bg-slate-800 p-10 w-1/4' >
-      <label htmlFor="title" className='font-bold text-sm'>titulo de tarea</label>
+      <form className='bg-slate-800 p-10 w-1/4 ' onSubmit={onSubmit} >
+        <label htmlFor="title" className='font-bold text-sm'>titulo de tarea</label>
         <input type="text" id='title'
-        className='border border-gray-400 p-2 mb-4 w-full' 
+          className='border border-gray-400 p-2 mb-4 w-full text-black'
           placeholder='titulo' />
-          <label htmlFor="description" className='font-bold text-sm'>Description</label>
-        <textarea className='border border-gray-400 p-2 mb-4 w-full' rows="3"
-        placeholder='contenido...' id='description'></textarea>
+        <label htmlFor="description" className='font-bold text-sm'>Description</label>
+        <textarea className='border border-gray-400 p-2 mb-4 w-full text-black' rows="3"
+          placeholder='contenido...' id='description'></textarea>
         <button type='submit'
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Crear</button>
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Crear</button>
       </form>
     </div>
   )
