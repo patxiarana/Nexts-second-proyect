6c4f7d3a0d64271ed9409846a0f0ce45a5eb5a4b
patxi <patxiarana05@gmail.com>
2024-01-10 08:56:00 -0300

    renderiando tareas

--- nextjs-prisma-crud/src/app/page.jsx
+++ nextjs-prisma-crud/src/app/page.jsx
@@ -1,9 +1,34 @@
+import {prisma} from '@/libs/prisma'
+ 
+async  function LoadTask (){
+  //Utilizando fetch
+  /*const res = await fetch('http://localhost:3000/api/task')
+  const data =  await res.json() 
+  console.log(data) */
+//utilizando prisma
+const tasks = await  prisma.task.findMany()
+return tasks
+ }
 
 
-  function HomePage() {
+
+async function HomePage() {
+
+   const tasks = await  LoadTask()
+   console.log(tasks)
     return (
-<div>Tareas</div>
-    )
+      <section className='container mx-auto'>
+<div className='grid grid-cols-3 gap-3 mt-10'>
+ { tasks.map(task => (
+  <div key={task.id} className='bg-slate-900 p-3 gap-3 hover:bg-slate-800 hover:cursor-pointer'>
+    <h3 className='font-bold text-2xl mb-2'>{task.title}</h3>
+    <p>{task.description}</p>
+    <p>{new Date(task.createdAT).toLocaleDateString()}</p>
+  </div>
+  )) }  
+</div>
+</section> 
+    )  
   }
 
 
